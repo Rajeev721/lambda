751445b837bcf38b967c7d0686a90100539b4d94
Rajeev721 <rajeev.suravajhula@gmail.com>
2024-02-20 21:43:48 -0600

    commit

--- ingest.py
+++ ingest.py
@@ -37,6 +37,15 @@ def update_dynamo(table_name, id, filename,start_time, update_date,message,run_s
                                            ':val3': {'S': run_status},
                                            }
                 )
+def get_latest_run(table_name):
+        dy = create_client('dynamodb')
+        response = dy.scan(TableName=table_name)
+        sorted_items = sorted(
+                response['Items'],
+                key=lambda item: item['start_time']['S'],
+                reverse=True
+                )
+        return sorted_items[0]['start_time']['N'][:15]
 def config_details(env):
         from configparser import ConfigParser
         conf = ConfigParser()
@@ -51,27 +60,31 @@ def file_ingest(env):
         # creating_objects()
         
         bucket_name, table_name = config_details(env)
-        print(bucket_name, table_name)
+        
+        latest_data = get_latest_run(table_name)
+
+        if latest_data:
+                file_name = f'{dt.strftime(dt.strptime(latest_data, "%Y%m%d%H%M%S") - td(hours = 1),"%Y-%m-%d-%-H") + ".json.gz"}'
+        else:
+                file_name = f'{dt.strftime(dt.now() - td(hours = 1),"%Y-%m-%d-%-H") + ".json.gz"}'
+
         s3 = create_client('s3')
-        n = 2
-        for i in range(n, 0 , -1):
-                file_name = f'{dt.strftime(dt.now() - td(hours = i),"%Y-%m-%d-%-H") + ".json.gz"}'
-                print(file_name)
-                id = str(uuid.uuid4())
-                start_time = dt.now().strftime("%Y%m%d%H%M%s")
-                insert_dynamo(table_name, id, file_name,  start_time)
-                try:
-                        res = rq.get(f'https://data.gharchive.org/{file_name}')
-                        a = s3.put_object(
-                                Bucket = bucket_name,
-                                Key = file_name,
-                                Body = res.content
-                        )
-                        message = str(a)
-                        run_status = 'Completed'
-                except Exception as e:
-                        run_status = 'Failed'
-                        message = str(e)
-                finally:
-                        update_dynamo(table_name, id, file_name,start_time, dt.now().strftime("%Y%m%d%H%M%s"), message, run_status)
+
+        id = str(uuid.uuid4())
+        start_time = dt.now().strftime("%Y%m%d%H%M%s")
+        insert_dynamo(table_name, id, file_name,  start_time)
+        try:
+                res = rq.get(f'https://data.gharchive.org/{file_name}')
+                a = s3.put_object(
+                        Bucket = bucket_name,
+                        Key = file_name,
+                        Body = res.content
+                )
+                message = str(a)
+                run_status = 'Completed'
+        except Exception as e:
+                run_status = 'Failed'
+                message = str(e)
+        finally:
+                update_dynamo(table_name, id, file_name,start_time, dt.now().strftime("%Y%m%d%H%M%s"), message, run_status)
         return message
